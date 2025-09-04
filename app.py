1165b5b92e0677b4e5ca2d08df3a182e5d2c8e7f
cabangwanarajacheck-max <cabangwanarajacheck@gmail.com>
2025-09-04 23:59:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import pytesseract
 from PIL import Image
 import io
 
+# arahkan langsung ke lokasi Tesseract
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
 app = Flask(__name__)
 
 @app.route("/ocr", methods=["POST"])
@@ -12,10 +15,13 @@ def ocr():
 
     file = request.files["file"]
     image = Image.open(io.BytesIO(file.read()))
-    text = pytesseract.image_to_string(image, lang="eng")  # bisa ganti "ind" kalau perlu
+    text = pytesseract.image_to_string(image, lang="eng+ind")  # bisa ganti "ind" kalau perlu
 
     return jsonify({"text": text})
 
 @app.route("/")
 def home():
     return "🚀 OCR API is running!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
